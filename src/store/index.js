0026e1ed238b342d817839889eb9ea01c8046486
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-06-18 16:01:10 +0300

    Создание store заказов

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,10 @@ import { createStore } from 'vuex'
 import ads from './ads'
 import user from './user'
 import shared from './shared'
+import orders from './orders'
 const store = createStore({
     modules:{
-        ads, user, shared
+        ads, user, shared, orders
     }
 })
 
