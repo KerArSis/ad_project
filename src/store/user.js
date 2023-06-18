f99336ce8092233c584db3a579f94211e4da0cbe
Арсен Керимов <113138301+KerArSis@users.noreply.github.com>
2023-06-18 12:23:28 +0300

    logout

--- src/store/user.js
+++ src/store/user.js
@@ -5,82 +5,72 @@ class User {
         this.password = password
     }
 }
-
-
+    
 export default {
-    state: {
+	state: {
         user: null
     },
-    mutations: {
+	mutations: {
         setUser(state, payload) {
             console.log(payload)
             state.user = payload
         }
     },
-    actions: {
-        async registerUser({
-            commit
-        }, {
-            email,
-            password
-        }) {
+	actions: {
+        async registerUser({commit},{email, password}) {
             commit('clearError')
             commit('setLoading', true)
             //Здесь выполняется запрос на сервер
             let isRequestOk = false
-            let promise = new Promise(function (resolve) {
-                setTimeout(() => resolve('Done'), 3000);
-            });
-
-
+            let promise = new Promise(function(resolve) {setTimeout(() => resolve('Done'), 3000);});
+            
             if (isRequestOk) {
-                await promise.then(() => {
+                await promise.then(()=> {
                     commit('setUser', new User(1, email, password))
                     commit('setLoading', false)
                 })
             } else {
-                await promise.then(() => {
+                await promise.then(()=> {
                     commit('setLoading', false)
                     commit('setError', 'Ошибка регистрации')
                     throw 'Упс... Ошибка регистрации'
                 })
             }
         },
-        async loginUser({
-            commit
-        }, {
-            email,
-            password
-        }) {
+
+        async loginUser({commit},{email, password}) {
             commit('clearError')
             commit('setLoading', true)
             //Здесь выполняется запрос на сервер
             let isRequestOk = false
-            let promise = new Promise(function (resolve) {
-                setTimeout(() => resolve('Done'), 3000);
-            });
-
-
+            let promise = new Promise(function(resolve) {setTimeout(() => resolve('Done'), 3000);});
+            
+            
             if (isRequestOk) {
-                await promise.then(() => {
+                await promise.then(()=> {
                     commit('setUser', new User(1, email, password))
                     commit('setLoading', false)
                 })
             } else {
-                await promise.then(() => {
+                await promise.then(()=> {
                     commit('setLoading', false)
                     commit('setError', 'Ошибка логина или пароля')
                     throw 'Упс... Ошибка логина или пароля'
                 })
             }
+        },
+
+        logoutUser({commit}) {
+            commit('setUser', null)
         }
     },
-    getters: {
+	getters: {
         user(state) {
-            return state.user
+            return state.user;
         },
-        isUserLoggedln(state) {
+        
+        isUserLoggedIn (state) {
             return state.user !== null
-        }
+        }        
     }
 }
